e563c2a6dcbc42fd27cf78457a0ba591bbe38483
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-03-12 13:52:35 +0900

    [Finish] book_commentの削除のときにモデルを２つ渡してエラー解消

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,4 +1,6 @@
 class BookCommentsController < ApplicationController
+  before_action :ensure_correct_comment_user, only: :destroy
+
   def create
     book_comment = current_user.book_comments.new(book_comment_params)
     book = Book.find(params[:book_id])
@@ -8,7 +10,7 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    book_comment = BookComment.find_by(id: params[:book_id], book_id: params[:id])
+    book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
     book_comment.destroy
     redirect_to book_path(book_comment.book_id)
   end
@@ -17,5 +19,12 @@ class BookCommentsController < ApplicationController
 
   def book_comment_params
     params.require(:book_comment).permit(:comment)
+  end
+
+  def ensure_correct_comment_user
+    book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    unless book_comment.user_id == current_user.id
+      redirect_to book_path(book_comment.book_id)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root 'homes#top'
-  get 'home/about' => 'homes#about'
+  root "homes#top"
+  get "home/about" => "homes#about"
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy] do
-    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
 end
